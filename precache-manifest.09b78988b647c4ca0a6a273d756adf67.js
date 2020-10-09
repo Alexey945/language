a9e3dc21c393c8f039c2d4b720982ba991e81ecc
Алексей <Alekseyshaposnikov7@gmail.com>
2020-10-09 20:08:01 +0300

    Updates

---
+++ precache-manifest.09b78988b647c4ca0a6a273d756adf67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1039504822acbb45ef124b9e20eaf88",
+    "url": "/language/index.html"
+  },
+  {
+    "revision": "50d6b721759a2ad9c1b8",
+    "url": "/language/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "143585dbe8124c8b8118",
+    "url": "/language/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "50d6b721759a2ad9c1b8",
+    "url": "/language/static/js/2.efdb7f0d.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/language/static/js/2.efdb7f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143585dbe8124c8b8118",
+    "url": "/language/static/js/main.d60705a2.chunk.js"
+  },
+  {
+    "revision": "2b59bca533b9134bb75c",
+    "url": "/language/static/js/runtime-main.b1c25d5a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/language/static/media/persik.4e1ec840.png"
+  }
+]);
